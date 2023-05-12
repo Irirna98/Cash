74ffd5e45b770777d03882d32b41b208ddd4094b
Irirna <burdina98@mail.ru>
2023-05-12 10:14:15 +0500

    init cash

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,20 +1,32 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.Assert.assertEquals;
+
 
 class CashbackHackServiceTest {
     @Test
-    void shouldReturn100IfAmountIs() {
+    public void shouldReturn100IfAmountIs() {
 
         CashbackHackService service = new CashbackHackService();
         int amount = 900;
         int expected = 100;
 
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturn0IfAmountIs() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+
 
         int actual = service.remain(amount);
-
         assertEquals(expected, actual);
     }
 }
